c1a9b0f55e40b2a6dba34fb17a7f90766c919883
Zeb <burroughszeb@me.com>
2020-04-10 12:05:39 -0500

    testing

--- commit_notifier.js
+++ commit_notifier.js
@@ -1,4 +1,4 @@
 const args = process.argv.slice(2);
-const json = JSON.parse(Buffer.from(args[0], "base64").toString());
-console.dir(args);
-console.dir(json);
+// const json = JSON.parse(Buffer.from(args[0], "base64").toString());
+console.log(process.env);
+// console.dir(json);
