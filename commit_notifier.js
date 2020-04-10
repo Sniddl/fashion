ce476e94df28f467f33366ffcd666e99926714a3
Zeb <burroughszeb@me.com>
2020-04-10 13:07:37 -0500

    Here is a summary
    This is a longer descriptino that I'm trying to test for the git hook.

--- commit_notifier.js
+++ commit_notifier.js
@@ -1,4 +1,7 @@
 const args = process.argv.slice(2);
+
+const eventContent = fs.readFileSync(process.env.GITHUB_EVENT_PATH, "utf8");
 // const json = JSON.parse(Buffer.from(args[0], "base64").toString());
 console.log(process.env);
+console.log(JSON.parse(eventContent));
 // console.dir(json);
