a8da73dbd3b4531b2c926b0a69f74d5a34d17709
Zack <zackbaldwin148@gmail.com>
2020-04-15 23:13:16 -0400

    less ugly nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,39 +24,32 @@
 </head>
 <body class="bg-gray-100 text-black">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                            <li class="nav-item">
-                                <a id="light" class="nav-link" href="#">Light</a>
-                            </li>
-                            <li class="nav-item">
-                                <a id="dark" class="nav-link" href="#">Dark</a>
-                            </li>
-                            <li class="nav-item">
-                                <a id="solar" class="nav-link" href="#">Solar</a>
-                            </li>
-                            <li class="nav-item">
-                                <a id="solarDark" class="nav-link" href="#">Solar Dark</a>
-                            </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <header class="lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2">
+            <div class="flex-1 flex justify-between items-center">
+              <a href="/">
+                <img class="rounded-full w-10 h-10 border-2 border-transparent" src="img/fashion_logo.png">
+            </a>
+          </div>
+        
+           <label for="menu-toggle" class="pointer-cursor lg:hidden block"><i class="fas fa-bars"></i><title>menu</title></label>
+          <input class="hidden" type="checkbox" id="menu-toggle" />
+        
+          <div class="hidden lg:flex lg:items-center lg:w-auto w-full" id="menu">
+            <nav>
+              <ul class="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+                <li><a id="light" class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Light</a></li>
+                <li><a id="dark" class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Dark</a></li>
+                <li><a id="solar" class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400" href="#">Solar</a></li>
+                <li><a id="solarDark" class="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400 lg:mb-0 mb-2" href="#">Solar Dark</a></li>
+              </ul>
+            </nav>
+            <a href="#" class="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
+              <img class="rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400" src="img/fashion_logo.png">
+            </a>
+        
+          </div>
+        
+          </header>
 
         <main class="py-4">
             @yield('content')
